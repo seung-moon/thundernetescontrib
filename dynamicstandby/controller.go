2c172a513c861c81bb269804c2543060be726796
Seung Moon <seungmoon@microsoft.com>
2022-09-20 14:41:55 -0400

    Add logic to get or create configmap

--- dynamicstandby/controller.go
+++ dynamicstandby/controller.go
@@ -3,14 +3,17 @@ package main
 import (
 	"context"
 
-	mpsv1alpha1 "github.com/playfab/thundernetes/pkg/operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sruntime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	mpsv1alpha1 "github.com/playfab/thundernetes/pkg/operator/api/v1alpha1"
 )
 
 var (
@@ -39,6 +42,24 @@ func (r *DynamicStandbyReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 
 	// TODO: add logic for dynamic standby
 
+	// check if correspoding configmap exists
+	var cfm corev1.ConfigMap
+	if err := r.Get(ctx, client.ObjectKey{Namespace: gsb.Namespace, Name: gsb.Name}, &cfm); err != nil {
+		if apierrors.IsNotFound(err) {
+			c, err := r.createConfigMap(ctx, &gsb)
+			if apierrors.IsConflict(err) {
+				log.Info("ConfigMap exists already")
+			} else if err != nil {
+				log.Error(err, "unable to create configmap")
+				return ctrl.Result{}, err
+			}
+			cfm = *c
+		} else {
+			log.Error(err, "unable to get configmap")
+			return ctrl.Result{}, err
+		}
+	}
+
 	return ctrl.Result{}, nil
 }
 
@@ -64,4 +85,29 @@ func (r *DynamicStandbyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&mpsv1alpha1.GameServerBuild{}).
 		Complete(r)
+}
+
+func (r *DynamicStandbyReconciler) createConfigMap(ctx context.Context, gsb *mpsv1alpha1.GameServerBuild) (*corev1.ConfigMap, error) {
+	cfm := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      gsb.Name,
+			Namespace: gsb.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(gsb, schema.GroupVersionKind{
+					Group:   mpsv1alpha1.GroupVersion.Group,
+					Version: mpsv1alpha1.GroupVersion.Version,
+					Kind:    "GameServerBuild",
+				}),
+			},
+		},
+		Data: map[string]string{
+			"BuildID": gsb.Spec.BuildID,
+		},
+	}
+
+	if err := r.Create(ctx, &cfm); err != nil {
+		return nil, err
+	}
+
+	return &cfm, nil
 }
